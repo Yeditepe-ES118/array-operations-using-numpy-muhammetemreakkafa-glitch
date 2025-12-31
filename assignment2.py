8fa0ac8857556041e0b712ea1d05579e1a50d917
muhammetemreakkafa-glitch <muhammetemre.akkafa@std.yeditepe.edu.tr>
2025-12-31 19:28:04 +0300

    assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,19 +1,22 @@
 import numpy as np
 
 def stat():
+    data = np.loadtxt('populations.txt', skiprows=1)
 
+    hare = data[:, 1]
 
-    data=np.loadtxt('populations.txt', skiprows=1)
+    min_year_hare = data[np.argmin(hare), 0]
 
-    hare=data[:,1]
+    lynx_avg = np.mean(data[:, 2])
 
-    min_year_hare=data[np.argmin(hare), 0]
+    # 5. madde: satır toplamı
+    row_sum = np.sum(data[:, 1:], axis=1)
 
-    lynx_avg=np.mean(data[:,2])
+    # data + yeni sütun
+    new_data = np.column_stack((data, row_sum))
 
-    new_data=np.sum(data[:,1:], axis=1)
+    # 6. madde: carrot < 40000 → 0
     new_data[new_data[:, 3] < 40000, 3] = 0
-
 
     return data, hare, min_year_hare, lynx_avg, new_data
 
@@ -22,3 +25,4 @@ def stat():
 
 
     
+
